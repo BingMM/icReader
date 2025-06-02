80ebb71a0be8d6c1ef31b4ed9315360706db00a8
bingMM <michael.madelaire@uib.no>
2025-06-02 08:00:24 +0200

    Adding init and fixing minimal working example.

---
+++ icreader/__init__.py
@@ -0,0 +1,8 @@
+# icreader/__init__.py
+
+from .conductanceimage import ConductanceImage
+
+__all__ = [
+    "ConductanceImage"
+]
+
--- scripts/test_load_conductance_image.py
+++ scripts/test_load_conductance_image.py
@@ -1,19 +1,20 @@
 #%% Import
 
 import matplotlib.pyplot as plt
-from icreader import conductanceimage
+from icreader import ConductanceImage
 
 #%% Paths
 
 base = '/Home/siv32/mih008/repos/icReader/'
+base = '/home/bing/Dropbox/work/code/repos/icReader/'
 path_in = base + 'example_data/or_0085.nc'
 
 #%% Load conductance Image
 
-cI = conductanceimage(path_in)
+cI = ConductanceImage(path_in)
 
 #%% Plot something
 
 plt.figure(figsize=(10,10))
-plt.imshow(cI.H[50])
+plt.imshow(cI.H[70])
 plt.colorbar()
